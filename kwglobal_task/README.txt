4ac02f19ead87e9d6d042f9c5f681bd0fd8e31ce
Ganesh <ganeshjadhav008@gmail.com>
2021-06-08 14:13:22 +0530

    code is fixed as per code standard

---
+++ kwglobal_task/README.txt
@@ -0,0 +1,40 @@
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Requirements
+ * Installation
+ * Permission
+ * Access form
+
+
+INTRODUCTION
+------------
+  A Kwglobal Task created to get a user name and log message
+
+
+REQUIREMENTS
+------------
+
+  No special requirements.
+
+
+INSTALLATION
+------------
+
+  * follow the following steps for installation
+    - go to /admin/modules 
+    - find Kwglobal Task module using filter 
+    - check checkbox and click on install
+
+
+PERMISSION
+-------------
+  * go to admin/people/permissions
+  * Find group Kwglobal Task
+  * add permission role wise
+
+ACCESS FORM
+-------------
+  * The menu will be enabled in the Tool block. Menu name: Task
+  * URL: /kwglobal-task access for directly
--- kwglobal_task/src/Form/kwglobalForm.php
+++ kwglobal_task/src/Form/kwglobalForm.php
@@ -1,64 +1,93 @@
-<?php  
-/**  
- * @file  
- * Contains Drupal\kwglobal_task\Form\kwglobalForm 
- */  
+<?php
 
-namespace Drupal\kwglobal_task\Form;  
-use Drupal\Core\Form\FormBase;  
-use Drupal\Core\Form\FormStateInterface;  
+namespace Drupal\Kwglobal_task\Form;
 
-class kwglobalForm extends FormBase {  
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
-  /**  
-   * {@inheritdoc}  
-   */  
-  public function getFormId() {  
-    return 'mail_notification';  
-  }  
+/**
+ * Provides a form for log & view data.
+ */
+class KwglobalForm extends FormBase {
 
-  /**  
-   * {@inheritdoc}  
-   */  
-  public function buildForm(array $form, FormStateInterface $form_state) {  
-   
+  /**
+   * The logger factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
+   * Constructs a logger object.
+   *
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory.
+   */
+  public function __construct(LoggerChannelFactoryInterface $logger_factory) {
+    $this->loggerFactory = $logger_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('logger.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'mail_notification';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
     $form['kwglobal_name'] = [
       '#title' => $this->t('Enter Your First Name'),
       '#type' => 'textfield',
-      '#placeholder' => $this->t('Enter First Name')
+      '#placeholder' => $this->t('Enter First Name'),
     ];
 
-    $form['actions']['submit'] = array(
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
       '#button_type' => 'primary',
-    );
+    ];
 
     return $form;
-  }  
+  }
 
   /**
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
 
-    if(!preg_match("/^([a-zA-Z']+)$/", $form_state->getValue('kwglobal_name'))) {
-      $form_state->setErrorByName("kwglobal_name", t('Entered name is not valid. Please enter valid name'));
+    if (!preg_match("/^([a-zA-Z']+)$/", $form_state->getValue('kwglobal_name'))) {
+      $form_state->setErrorByName("kwglobal_name", $this->t('Entered name is not valid. Please enter valid name'));
     }
 
   }
 
-  /**  
-   * {@inheritdoc}  
-   */  
-  public function submitForm(array &$form, FormStateInterface $form_state) {  
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
 
-    $kwglobal_name =  !empty($form_state->getValue('kwglobal_name')) ? $form_state->getValue('kwglobal_name') : NULL;
-    //set message here
-    drupal_set_message(t('Hi %name',['%name' => $kwglobal_name]), 'status', TRUE);
-    
-    // Logs a notice
-    \Drupal::logger('kwglobal_task')->notice(t('Submitted Name: %name', ['%name' => $kwglobal_name]));
+    $kwglobal_name = !empty($form_state->getValue('kwglobal_name')) ? $form_state->getValue('kwglobal_name') : NULL;
+    // Set drupal message here.
+    drupal_set_message($this->t('Hi %name', ['%name' => $kwglobal_name]), 'status', TRUE);
+
+    // Log details.
+    $this->loggerFactory->get('kwglobal_task')->notice($this->t('Submitted Name: %name', ['%name' => $kwglobal_name]));
 
   }
+
 }
